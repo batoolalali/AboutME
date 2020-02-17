aab195ed777d91c16975ecbdd39cd5c225034df1
batoolalali <abdullahbatool93@gmail.com>
2020-02-17 16:41:54 +0200

    Built the repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# AboutME
+ A Guessing Game about me
+It's something like a game gives the user yes/no questions about me, and returns an alert to tell him if the answer is true or false.
---
+++ js/app.js
@@ -0,0 +1,90 @@
+"use strict";
+
+var userName = prompt("What is your name ?");
+alert("weclome " + userName);
+document.write("<span> Welcome </span>")
+document.write('<span>'+userName+'</span>')
+
+
+var Age = prompt("Guess, Is my age more than 30? ");
+
+//console.log(myAge);
+
+if (Age.toLowerCase() === 'y' || Age.toLowerCase()=== 'yes')
+{
+
+    alert('you guess is false');
+
+}else if (Age.toLowerCase() === 'n' || Age.toLowerCase()=== 'no')
+{
+    alert('your guess is true');
+} else {  alert('wronge input');}
+
+
+
+
+
+var movies = prompt("Guess, Do I love watching movies? ");
+
+//console.log(movies);
+
+if (movies.toLowerCase() === 'y' || movies.toLowerCase()=== 'yes')
+{
+
+    alert('you guess is true');
+
+}else if (movies.toLowerCase() === 'n' || movies.toLowerCase()=== 'no')
+{
+    alert('your guess is false');
+} else {  alert('wronge input');}
+
+
+var car = prompt("Guess, Do I have a car? ");
+
+//console.log(car);
+
+if (car.toLowerCase() === 'y' || car.toLowerCase()=== 'yes')
+{
+
+    alert('you guess is false');
+
+}else if (car.toLowerCase() === 'n' || car.toLowerCase()=== 'no')
+{
+    alert('your guess is true');
+} else {  alert('wronge input');}
+
+
+var born = prompt("Guess, Do I born in Jordan? ");
+
+//console.log(born);
+
+if (born.toLowerCase() === 'y' || born.toLowerCase()=== 'yes')
+{
+
+    alert('you guess is true');
+
+}else if (born.toLowerCase() === 'n' || born.toLowerCase()=== 'no')
+{
+    alert('your guess is false');
+} else {  alert('wronge input');}
+
+
+var pet = prompt("Guess, Do I have a pet? ");
+
+//console.log(pet);
+
+if (pet.toLowerCase() === 'y' || pet.toLowerCase()=== 'yes')
+{
+
+    alert('you guess is false');
+
+}else if (pet.toLowerCase() === 'n' || pet.toLowerCase()=== 'no')
+{
+    alert('your guess is true');
+} else {  alert('wronge input');}
+
+
+
+
+
+
