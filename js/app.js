f98713a07c26ccbe7b02cccbc65b9880ac670542
batoolalali <abdullahbatool93@gmail.com>
2020-02-18 11:54:28 +0200

    commit lab3

--- js/app.js
+++ js/app.js
@@ -1,87 +1,57 @@
-"use strict";
+'use strict';
 
-var userName = prompt("What is your name ?");
-alert("weclome " + userName);
-document.write("<span> Welcome </span>")
-document.write('<span>'+userName+'</span>')
+var userName = prompt('What is your name ?');
+alert('weclome ' + userName);
+document.write('<span> Welcome </span>');
+document.write('<span>' + userName + '</span>');
 
 
-var Age = prompt("Guess, Is my age more than 30? ");
+var Age = prompt('Guess, Is my age more than 30? ');
 
 //console.log(myAge);
 
-if (Age.toLowerCase() === 'y' || Age.toLowerCase()=== 'yes')
-{
+if (Age.toLowerCase() === 'y' || Age.toLowerCase() === 'yes') {alert('you guess is false');
 
-    alert('you guess is false');
-
-}else if (Age.toLowerCase() === 'n' || Age.toLowerCase()=== 'no')
-{
-    alert('your guess is true');
-} else {  alert('wronge input');}
+} else if (Age.toLowerCase() === 'n' || Age.toLowerCase() === 'no') {alert('your guess is true');
+} else { alert('wronge input'); }
 
 
 
-
-
-var movies = prompt("Guess, Do I love watching movies? ");
-
+var movies = prompt('Guess, Do I love watching movies? ');
 //console.log(movies);
 
-if (movies.toLowerCase() === 'y' || movies.toLowerCase()=== 'yes')
-{
+if (movies.toLowerCase() === 'y' || movies.toLowerCase() === 'yes') {alert('you guess is true');
 
-    alert('you guess is true');
-
-}else if (movies.toLowerCase() === 'n' || movies.toLowerCase()=== 'no')
-{
-    alert('your guess is false');
-} else {  alert('wronge input');}
+} else if (movies.toLowerCase() === 'n' || movies.toLowerCase() === 'no') {alert('your guess is false');
+} else { alert('wronge input'); }
 
 
-var car = prompt("Guess, Do I have a car? ");
+var car = prompt('Guess, Do I have a car? ');
 
 //console.log(car);
 
-if (car.toLowerCase() === 'y' || car.toLowerCase()=== 'yes')
-{
+if (car.toLowerCase() === 'y' || car.toLowerCase() === 'yes') {alert('you guess is false');
 
-    alert('you guess is false');
+} else if (car.toLowerCase() === 'n' || car.toLowerCase() === 'no') {alert('your guess is true');
+} else { alert('wronge input'); }
 
-}else if (car.toLowerCase() === 'n' || car.toLowerCase()=== 'no')
-{
-    alert('your guess is true');
-} else {  alert('wronge input');}
-
-
-var born = prompt("Guess, Did I born in Jordan? ");
-
+var born = prompt('Guess, Did I born in Jordan? ');
 //console.log(born);
 
-if (born.toLowerCase() === 'y' || born.toLowerCase()=== 'yes')
-{
+if (born.toLowerCase() === 'y' || born.toLowerCase() === 'yes') {alert('you guess is true');
 
-    alert('you guess is true');
-
-}else if (born.toLowerCase() === 'n' || born.toLowerCase()=== 'no')
-{
-    alert('your guess is false');
-} else {  alert('wronge input');}
+} else if (born.toLowerCase() === 'n' || born.toLowerCase() === 'no') {alert('your guess is false');
+} else { alert('wronge input'); }
 
 
-var pet = prompt("Guess, Do I have a pet? ");
+var pet = prompt('Guess, Do I have a pet? ');
 
 //console.log(pet);
 
-if (pet.toLowerCase() === 'y' || pet.toLowerCase()=== 'yes')
-{
+if (pet.toLowerCase() === 'y' || pet.toLowerCase() === 'yes') {alert('you guess is false');
 
-    alert('you guess is false');
-
-}else if (pet.toLowerCase() === 'n' || pet.toLowerCase()=== 'no')
-{
-    alert('your guess is true');
-} else {  alert('wronge input');}
+} else if (pet.toLowerCase() === 'n' || pet.toLowerCase() === 'no') {alert('your guess is true');
+} else { alert('wronge input'); }
 
 
 
